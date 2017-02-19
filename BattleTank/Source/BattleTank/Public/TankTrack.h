531200f909991b31da01f0d5ad15e4b5753050f2
Lincoln <sylenazeur@gmail.com>
2017-02-19 17:36:38 -0600

    BT15 - Added physics and forward motion force to the tank to allow it to move forward and backward on the track.

---
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Components/StaticMeshComponent.h"
+#include "TankTrack.generated.h"
+
+/**
+ * Used for driving force of the tracks for movement
+ */
+UCLASS(meta = (BlueprintSpawnableComponent))
+class BATTLETANK_API UTankTrack : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+public:
+	// sets throttle between -1 and 1
+	UFUNCTION(BlueprintCallable, Category = Input)
+	void SetThrottle(float Throttle);
+	
+	// Max force per track in Newtons
+	UPROPERTY(EditDefaultsOnly)
+	float TrackMaxDrivingForce = 165000; // Set to 66 tonne tank, and 9m/s^2 acceleration
+};
